3a5ed2015eb6dba177e610f66f167dbab013411e
JL <jeremyli.gitemail@gmail.com>
2023-11-05 09:16:21 -0600

    more work on level generation

--- src/level.rs
+++ src/level.rs
@@ -1,5 +1,6 @@
 use std::mem::size_of;
 use std::os::raw::c_void;
+use rand::Rng;
 
 //The distance of the level from the camera
 pub const LEVEL_Z: f32 = -8.0;
@@ -75,7 +76,7 @@ impl Level {
         }
     }
 
-    fn generate_room(&mut self, room_x: u32, room_y: u32) {
+    fn create_room(&mut self, room_x: u32, room_y: u32) {
         for x in room_x..(room_x + ROOM_SIZE) {
             for y in room_y..(room_y + ROOM_SIZE) {
                 self.set_tile(x, y, Tile::Air); 
@@ -88,10 +89,46 @@ impl Level {
 
         for room_x in 0..4 {
             for room_y in 0..4 {
-                level.generate_room(room_x * (ROOM_SIZE + 1) + 1, room_y * (ROOM_SIZE + 1) + 1);
+                level.create_room(room_x * (ROOM_SIZE + 1) + 1, room_y * (ROOM_SIZE + 1) + 1);
             }
         }
+        
+        let mut rng = rand::thread_rng();
 
+        for room_y in 0..4 {
+            for room_x in 0..3 { 
+                if rng.gen::<u32>() % 4 == 0 { 
+                    //Randomly combine rooms
+                    (((room_y) * (ROOM_SIZE + 1) + 1)..
+                    ((room_y) * (ROOM_SIZE + 1) + ROOM_SIZE + 1))
+                        .for_each(|y| level.set_tile((room_x + 1) * (ROOM_SIZE + 1), y, Tile::Air));
+                } else {
+                    //Create doorways between rooms
+                    ((room_y * (ROOM_SIZE + 1) + 1)..((room_y * (ROOM_SIZE + 1) + 4)))
+                        .for_each(|y| level.set_tile((room_x + 1) * (ROOM_SIZE + 1), y, Tile::Air));
+                } 
+            }
+
+            //Create an exit that will bring the player to the next floor
+            let exit_x = if room_y == 0 {
+                rng.gen::<u32>() % 3 + 1 
+            } else {
+                rng.gen::<u32>() % 4
+            };
+
+            (((exit_x) * (ROOM_SIZE + 1) + ROOM_SIZE / 2 - 1)..
+            ((exit_x) * (ROOM_SIZE + 1) + ROOM_SIZE / 2 + 3))
+                .for_each(|x| level.set_tile(x, (room_y + 1) * (ROOM_SIZE + 1), Tile::Air));
+
+            //Attempt to generate a second possible exit to the next floor
+            if rng.gen::<u32>() % 2 == 0 && room_y > 0 && room_y < 3 {
+                let exit_x = rng.gen::<u32>() % 4;
+
+                (((exit_x) * (ROOM_SIZE + 1) + ROOM_SIZE / 2 - 1)..
+                ((exit_x) * (ROOM_SIZE + 1) + ROOM_SIZE / 2 + 3))
+                    .for_each(|x| level.set_tile(x, (room_y + 1) * (ROOM_SIZE + 1), Tile::Air));
+            }
+        } 
         
         level
     }

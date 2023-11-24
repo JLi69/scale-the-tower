db5390019e15a4d317721ebab2b9efc8e3042564
JL <jeremyli.gitemail@gmail.com>
2023-11-24 10:54:30 -0600

    changed room templates to be stored as pngs

--- src/level/room_template.rs
+++ src/level/room_template.rs
@@ -4,6 +4,7 @@ use super::ROOM_SIZE;
 use core::slice::Iter;
 use std::fs::File;
 use std::io::Read;
+use crate::gfx::load_image_pixels;
 
 /*
  * There are 4 types of room templates:
@@ -32,102 +33,92 @@ pub struct RoomTemplate {
     spawns: Vec<Spawn>,
 }
 
-//Attempts to convert an ascii character
-//to a tile, if the character is not
+//Attempts to convert a pixel
+//to a tile, if the pixel is not
 //recognized then this function returns None
-fn ascii_to_tile(ch: u8) -> Option<Tile> {
-    match ch {
-        b'#' => Some(Tile::Brick),
-        b'.' => Some(Tile::Air),
-        b'|' => Some(Tile::Ladder),
-        b'@' => Some(Tile::BrickTile),
-        b'=' => Some(Tile::BrickTile2),
-        b'!' => Some(Tile::Lava),
-        b'^' => Some(Tile::Spikes),
+//pixels are of the format alpha, b, g, r
+fn pixel_to_tile(pixel: u32) -> Option<Tile> {
+    match pixel {
+        0x00000000 => Some(Tile::Air),
+        0xffaaaaaa => Some(Tile::Brick),
+        0xff002848 => Some(Tile::Ladder),
+        0xff666666 => Some(Tile::BrickTile),
+        0xff444444 => Some(Tile::BrickTile2),
+        0xff0000ff => Some(Tile::Lava),
+        0xffffffff => Some(Tile::Spikes),
         _ => None,
     }
 }
 
-fn ascii_to_spawn(ch: u8) -> Option<SpawnType> {
-    match ch {
-        b'g' => Some(SpawnType::MaybeTreasure),
-        b'G' => Some(SpawnType::Treasure),
-        b'e' => Some(SpawnType::MaybeEnemy),
-        b'E' => Some(SpawnType::Enemy),
-        b'?' => Some(SpawnType::Pickup),
+fn pixel_to_spawn(pixel: u32) -> Option<SpawnType> {
+    match pixel {
+        0xff008888 => Some(SpawnType::MaybeTreasure),
+        0xff00ffff => Some(SpawnType::Treasure),
+        0xff008800 => Some(SpawnType::MaybeEnemy),
+        0xff00ff00 => Some(SpawnType::Enemy),
+        0xffff00ff => Some(SpawnType::Pickup),
         _ => None,
     }
 }
 
-fn ascii_to_background_tile(ch: u8) -> Option<BackgroundTile> {
-    match ch {
-        b'b' => Some(BackgroundTile::BannerTop),
-        b's' => Some(BackgroundTile::SkullDecoration),
-        b'w' => Some(BackgroundTile::Window),
-        b'W' => Some(BackgroundTile::BarredWindow),
-        b'p' => Some(BackgroundTile::Painting1),
-        b'*' => Some(BackgroundTile::BigWindowTop),
+fn pixel_to_background_tile(pixel: u32) -> Option<BackgroundTile> {
+    match pixel {
+        0xffff4848 => Some(BackgroundTile::BannerTop),
+        0xff222222 => Some(BackgroundTile::SkullDecoration),
+        0xffffff00 => Some(BackgroundTile::Window),
+        0xff888800 => Some(BackgroundTile::BarredWindow),
+        0xffff8800 => Some(BackgroundTile::Painting1),
+        0xffffaa66 => Some(BackgroundTile::BigWindowTop),
         _ => None,
     }
 }
 
 impl RoomTemplate {
     pub fn load_from_file(path: &str) -> Result<Self, String> {
-        match File::open(path) {
-            Ok(mut file) => {
-                let mut template = RoomTemplate {
-                    tiles: [Tile::Air; (ROOM_SIZE * ROOM_SIZE) as usize],
-                    background_tiles: [BackgroundTile::Wall; (ROOM_SIZE * ROOM_SIZE) as usize],
-                    spawns: Vec::new(),
-                };
+        let mut template = RoomTemplate {
+            tiles: [Tile::Air; (ROOM_SIZE * ROOM_SIZE) as usize],
+            background_tiles: [BackgroundTile::Wall; (ROOM_SIZE * ROOM_SIZE) as usize],
+            spawns: Vec::new(),
+        };
 
-                let mut buf = [0u8; ((ROOM_SIZE + 1) * ROOM_SIZE) as usize];
-                file.read(&mut buf).map_err(|e| e.to_string())?;
+        let (buf, _) = load_image_pixels(path)?;
 
-                let mut x = 0;
-                let mut y = ROOM_SIZE - 1;
+        let mut x = 0;
+        let mut y = ROOM_SIZE - 1;
 
-                buf.iter()
-                    .filter(|ch| ch.is_ascii_graphic())
-                    .for_each(|ch| {
-                        template.set_tile(x, y, ascii_to_tile(*ch).unwrap_or(Tile::Air));
-                        template.set_background_tile(
-                            x,
-                            y,
-                            ascii_to_background_tile(*ch).unwrap_or(BackgroundTile::Wall),
-                        );
+        buf.iter()
+            .for_each(|pixel| {
+                template.set_tile(x, y, pixel_to_tile(*pixel).unwrap_or(Tile::Air));
+                template.set_background_tile(
+                    x,
+                    y,
+                    pixel_to_background_tile(*pixel).unwrap_or(BackgroundTile::Wall),
+                );
 
-                        if template.get_background_tile(x, y + 1) == BackgroundTile::BannerTop {
-                            template.set_background_tile(x, y, BackgroundTile::BannerBottom);
-                        } else if template.get_background_tile(x, y + 1)
-                            == BackgroundTile::BigWindowTop
-                        {
-                            template.set_background_tile(x, y, BackgroundTile::BigWindowBottom);
-                        }
+                if template.get_background_tile(x, y + 1) == BackgroundTile::BannerTop {
+                    template.set_background_tile(x, y, BackgroundTile::BannerBottom);
+                } else if template.get_background_tile(x, y + 1)
+                    == BackgroundTile::BigWindowTop
+                {
+                    template.set_background_tile(x, y, BackgroundTile::BigWindowBottom);
+                }
 
-                        if let Some(t) = ascii_to_spawn(*ch) {
-                            template.spawns.push(Spawn {
-                                spawn_type: t,
-                                tile_x: x,
-                                tile_y: y,
-                            })
-                        }
+                if let Some(t) = pixel_to_spawn(*pixel) {
+                    template.spawns.push(Spawn {
+                        spawn_type: t,
+                        tile_x: x,
+                        tile_y: y,
+                    })
+                }
 
-                        x += 1;
-                        if x >= ROOM_SIZE && y > 0 {
-                            x = 0;
-                            y -= 1;
-                        }
-                    });
+                x += 1;
+                if x >= ROOM_SIZE && y > 0 {
+                    x = 0;
+                    y -= 1;
+                }
+            });
 
-                Ok(template)
-            }
-            Err(msg) => {
-                eprintln!("Failed to open: {path}");
-                eprintln!("{msg}");
-                Err(msg.to_string())
-            }
-        }
+        Ok(template)
     }
 
     pub fn get_tile(&self, x: u32, y: u32) -> Tile {
